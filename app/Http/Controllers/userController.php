b220171eff2b784806eab596687714cc386e321b
Nadim <nahlenadim@gmail.com>
2022-06-07 18:17:48 +0300

    add- getUsers API

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Users;
 
 class userController extends Controller
 {
-    //
+    public function getUsers($id = null){
+        $users = users::all();
+        return response()->json([
+            "status" => "success",
+            "users" => $users
+        ],200);
+     }
 }
